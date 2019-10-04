eff8c1f94099d9dd1c6e3c96ffd768aef4fc2de9
Doğukan Kotan <me@dogukankotan.com>
2019-10-05 01:14:02 +0300

    standards index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,11 @@
 var express = require('express')
 var router = express.Router()
-var { models } = require('../models')
-
-async function run () {
-  // Find all data
-
-  const docs = await models.Record.find({ counts: { $lte: 100 } })
-  console.log(docs)
-  return docs
-}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var docs = run()
-  res.send(docs)
+  res.send({
+    message: 'Hello World'
+  })
 })
 
 module.exports = router
