b76b5a4f95226dd7f30890c2c86c500031f4ee48
Doğukan Kotan <me@dogukankotan.com>
2019-10-05 15:01:19 +0300

    filter query when post index then bind

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,41 @@
 var express = require('express')
 var router = express.Router()
+var { models } = require('../models')
+
+async function query(req) {
+  const filteredRecords = await models.Record.aggregate([
+    {
+      "$project": {
+        "key": 1,
+        "createdAt": 1,
+        "totalCount": {
+          "$sum": "$counts"
+        },
+      }
+    },
+    {
+      $match: {
+        $and: [{
+          createdAt: {
+            $gte:
+              new Date(req.startDate), $lte: new Date(req.endDate)
+          }
+        }, {
+          totalCount: {
+            $gte: req.minCount,
+            $lte: req.maxCount
+          }
+        }]
+      }
+    }
+  ])
+  return filteredRecords;
+}
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send({
-    message: 'Hello World'
-  })
+router.post('/', function (req, res, next) {
+  const objects = query(req.body);
+  objects.then(res.send.bind(res));
 })
 
 module.exports = router
