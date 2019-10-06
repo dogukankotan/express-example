762fd42047a456b86dec79221d141bb0413c09ed
Doğukan Kotan <me@dogukankotan.com>
2019-10-06 13:08:39 +0300

    npx standard fix

--- src/models/record.js
+++ src/models/record.js
@@ -29,8 +29,8 @@ recordSchema.statics.findByDateAndCounts = async function (startDate, endDate, m
         key: 1,
         createdAt: 1,
         totalCount: {
-          $sum: "$counts"
-        },
+          $sum: '$counts'
+        }
       }
     },
     {
@@ -38,7 +38,8 @@ recordSchema.statics.findByDateAndCounts = async function (startDate, endDate, m
         $and: [{
           createdAt: {
             $gte:
-              new Date(startDate), $lte: new Date(endDate)
+              new Date(startDate),
+            $lte: new Date(endDate)
           }
         }, {
           totalCount: {
@@ -48,8 +49,8 @@ recordSchema.statics.findByDateAndCounts = async function (startDate, endDate, m
         }]
       }
     }
-  ]);
-};
+  ])
+}
 
 const Record = mongoose.model('Record', recordSchema)
 
