12e626eeef4064b46135ff87b68cc7977048d1c1
Doğukan Kotan <me@dogukankotan.com>
2019-10-06 13:09:06 +0300

    get method from model

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,40 +2,17 @@ var express = require('express')
 var router = express.Router()
 var { models } = require('../models')
 
-async function query(req) {
-  const filteredRecords = await models.Record.aggregate([
-    {
-      "$project": {
-        "key": 1,
-        "createdAt": 1,
-        "totalCount": {
-          "$sum": "$counts"
-        },
-      }
-    },
-    {
-      $match: {
-        $and: [{
-          createdAt: {
-            $gte:
-              new Date(req.startDate), $lte: new Date(req.endDate)
-          }
-        }, {
-          totalCount: {
-            $gte: req.minCount,
-            $lte: req.maxCount
-          }
-        }]
-      }
-    }
-  ])
-  return filteredRecords;
-}
-
 /* GET home page. */
 router.post('/', function (req, res, next) {
-  const objects = query(req.body);
-  objects.then(res.send.bind(res));
+  const body = req.body
+  try {
+    var objects = models.Record.findByDateAndCounts(body.startDate, body.endDate, body.minCount, body.maxCount)
+  } catch (err) {
+    console.log(err)
+  }
+  objects.then(
+    res.send.bind(res)
+  )
 })
 
 module.exports = router
